98d90ecc2ac5dc3485c1d700b965e771220a11d5
dbw <794363172@qq.com>
2022-08-12 16:20:17 +0800

    模型为SVC，运用了归一化，网格搜索。数据得分为0.97777

---
+++ iris(svm).py
@@ -0,0 +1,42 @@
+"""
+iris(svm)~
+
+Author:dbw
+Date：2022/8/12
+"""
+import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import Normalizer
+from sklearn.svm import SVC
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import GridSearchCV
+from sklearn import svm
+#导入数据，分训练集和测试集
+iris = load_iris()
+iris_X = iris.data#X为数据
+iris_Y = iris.target#Y为特征
+X_train, X_test, y_train, y_test = train_test_split(iris_X, iris_Y, test_size=0.3)
+#归一化
+scaler = Normalizer().fit(X_train)
+normalized_X = scaler.transform(X_train)
+normalized_X_test = scaler.transform(X_test)
+#网格搜索
+svc = svm.SVC()
+params = [{'kernel':['linear'], 'C':[1, 10, 100, 1000]},
+    {'kernel':['poly'], 'C':[1], 'degree':[2, 3]},
+    {'kernel':['rbf'], 'C':[1,10,100], 'gamma':[1, 0.1, 0.01]}]
+gs = GridSearchCV(estimator=svc,param_grid=params, cv=2)
+gs.fit(X_train,y_train)
+print('最优参数: ',gs.best_params_)
+print('最佳性能: ', gs.best_score_)
+print('最佳模型: ',gs.best_estimator_)
+
+clf = svm.SVC(kernel='rbf',gamma=0.1,                      # 核函数
+             decision_function_shape='ovo',      # one vs one 分类问题
+             C=10)
+clf.fit(X_train,y_train)
+# print('测试数据得分: {:.2f}'.format(clf.score(X_test, y_test)))
+
+score=clf.score(X_test, y_test)
+print('测试数据得分: ',score)

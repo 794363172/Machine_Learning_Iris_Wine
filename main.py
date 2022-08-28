0736cfa10a4701eed7945e7d1b1de9ea53b3c307
dbw <794363172@qq.com>
2022-08-28 17:23:34 +0800

    更换了归一化方法，使用MinMaxScaler，结果仍然是0.74

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import numpy as np
-from sklearn.neighbors import kneighbors_graph, KNeighborsClassifier
 from sklearn.datasets import load_wine
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
-
-wine = load_wine()
-X_train, X_test,y_train,y_test = train_test_split(wine.data,wine.target,
-                                                 test_size=0.3,random_state=1)
-knn = KNeighborsClassifier(n_neighbors=1)
-knn.fit(X_train, y_train)
-print('测试数据得分: {:.2f}'.format(knn.score(X_test, y_test)))
+from sklearn.neighbors import KNeighborsClassifier
+wine=load_wine()
+x_train,x_test,y_train,y_test = train_test_split(wine.data, wine.target,test_size=0.3,random_state=1)
+scalar = MinMaxScaler()
+X_train = scalar.fit_transform(x_train)
+X_test = scalar.fit_transform(x_test)
+knn = KNeighborsClassifier(n_neighbors=1,weights='uniform')
+knn.fit(x_train, y_train)
+print('测试数据得分: {:.2f}'.format(knn.score(x_test, y_test)))
